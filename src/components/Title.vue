36f878dc26a90010c200fe600ab79f764eaec882
FraneSpralja <spralja.frane@gmail.com>
2022-02-26 13:56:48 -0300

    Desafio Latam Vue Cli desafio 2

---
+++ src/components/Title.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>To do list</h1>
+</template>
+
+
+
+<script>
+export default {
+    name: 'Title'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,57 @@
+<template>
+    <Title />
+    <form id="Form" @submit.prevent>
+
+        <input id="lista" v-model="lista" type="text">
+        
+        <button type="submit" @click="addToDoList">Agregar</button>
+
+    </form>
+
+    <br><br>
+
+    <div v-for="(item, index) in todo_list" :key="item.id">
+        
+        {{index + 1}} - {{item}}
+
+    </div>
+</template>
+
+<script>
+
+import Title from './Title.vue'
+
+export default {
+    name: 'TodoList',
+    components: {
+        Title,
+    },
+    data () {
+        return {
+            lista: "",
+            todo_list: [],
+        }
+    },
+    methods: {
+        preventForm(){
+            const form = document.querySelector('#Form');
+            form.preventDefault();
+        },
+        funciona() {
+            console.log(this.lista)
+        },
+
+        addToDoList(){
+            const addLista = document.querySelector('#lista');
+            if(addLista.value !== ""){
+                this.todo_list.push(this.lista)
+            }else
+                console.log("No has agregado lista")
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
